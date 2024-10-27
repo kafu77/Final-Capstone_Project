489d061413b5cfc5f3958fd6ead8f72fccc2693e
kafu77 <kafubere99@gmail.com>
2024-10-26 22:24:43 -0700

    requirenments setup is done

---
+++ template.py
@@ -0,0 +1,53 @@
+import os
+from pathlib import Path
+import logging
+
+#Configure logging
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
+
+#Creating a source (SRC) folder
+project_name = 'CapstoneProject'
+
+#list of source files to be processed
+
+list_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/Data_ingestion.py",
+    f"src/{project_name}/components/Data_validation.py",
+    f"src/{project_name}/components/Data_preprocessing.py",    
+    f"src/{project_name}/components/data_transformation.py",
+    f"src/{project_name}/components/model_training.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/entity/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "main.py",
+    "requirements.txt",
+    "setup.py",
+    
+   
+            
+    
+]
+
+
+for filepath in list_files:
+    filepath= Path(filepath)
+    fileDir, filename =os.path.split(filepath)
+    
+    if fileDir != "":
+        os.makedirs(fileDir, exist_ok=True)
+        logging.info(f"Creating directory: {fileDir}:for the file {filename}")
+    
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"Created empty file: {filepath}")
+    else:
+        logging.info(f"{filepath} already exists")
